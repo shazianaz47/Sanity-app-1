969489f788b87fdf73fca0dca916c0099995b5f9
shazia naz <shazianaz2417@gmail.com>
2024-12-25 04:52:30 +0500

    first commit

---
+++ src/sanity/product.ts
@@ -0,0 +1,29 @@
+// sanity/product.ts
+export const product = {
+    name: 'product',
+    type: 'document',
+    title: 'Product',
+    fields: [
+        {
+            name: 'productName',
+            type: 'string',
+            title: 'Product Name'
+        },
+        {
+            name: 'price',
+            type: 'number',
+            title: 'Price'
+        },
+        {
+            name: 'image',
+            type: 'image',
+            title: 'Image'
+        },
+        {
+            name: 'description',
+            type: 'string',
+            title: 'Description'
+        }
+    
+    ]
+}
